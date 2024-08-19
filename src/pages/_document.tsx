abeb4b261c32cd1038d78b6601cc9e722acebb9c
lucastenani <lucas.tenani3@gmail.com>
2024-08-19 13:56:05 -0300

    build - Install and configure Stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
+import { getCssText } from '../styles'
+
 export default function Document() {
   return (
     <Html>
@@ -14,8 +16,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
-      </Head>
 
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  css,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {},
+  },
+})
