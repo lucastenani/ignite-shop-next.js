fe9a6e9195d07bd684737dcae9357baf0ee9b0a5
lucastenani <lucas.tenani3@gmail.com>
2024-08-20 12:04:57 -0300

    feat - Build product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,31 @@
 import { useRouter } from 'next/router'
 
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product {query.id}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Cmiseta x</h1>
+        <span>r$ 79,99</span>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+          corporis animi, veniam exercitationem voluptates commodi corrupti ab
+          ratione provident. Quaerat doloremque in incidunt unde, eaque
+          laboriosam. Tempore et at dolores!
+        </p>
+
+        <button>Buy now</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,85 @@
+import { styled } from '..'
+
+export const ProductContainer = styled('section', {
+  maxWidth: 1180,
+  margin: '0 auto',
+  padding: '1rem .5rem',
+
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+  alignItems: 'center',
+  gap: '2rem',
+
+  '@bp2': {
+    gridTemplateColumns: '1fr 1fr',
+    alignItems: 'stretch',
+    gap: '4rem',
+  },
+})
+
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 576,
+  height: 400,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  padding: '0.25rem',
+  margin: '0 auto',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: { objectFit: 'cover' },
+
+  '@bp2': {
+    height: 656,
+  },
+})
+
+export const ProductDetails = styled('article', {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+
+  h1: { fontSize: '$xl', color: '$gray300', '@bp1': { fontSize: '$2xl' } },
+
+  span: {
+    marginTop: '1rem',
+    fontSize: '$xl',
+    color: '$primary',
+
+    display: 'block',
+
+    '@bp1': { fontSize: '$2xl' },
+  },
+
+  p: {
+    marginTop: '1.5rem',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    color: '$gray300',
+    textAlign: 'center',
+
+    '@bp1': { marginTop: '2.5rem' },
+    '@bp2': { textAlign: 'left' },
+  },
+
+  button: {
+    marginTop: '1rem',
+    background: '$primary',
+    border: 0,
+    width: '100%',
+    color: '$white',
+    borderRadius: 8,
+    padding: '0.75rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+
+    '&:hover': { background: '$primaryLight' },
+
+    '@bp2': { marginTop: 'auto', padding: '1.25rem', fontSize: '$md' },
+  },
+
+  '@bp2': { alignItems: 'flex-start', gap: '1rem' },
+})
