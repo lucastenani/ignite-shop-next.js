b26ddb8147fb84e1092e686a243bd83a8f9937f1
lucastenani <lucas.tenani3@gmail.com>
2024-08-20 14:38:31 -0300

    feat - Integrate product page with Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,31 +1,65 @@
-import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import Stripe from 'stripe'
 
+import { stripe } from '../../lib/stripe'
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from '../../styles/pages/product'
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description?: string
+  }
+}
 
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} alt={`${product.imageUrl} Image.`} />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Cmiseta x</h1>
-        <span>r$ 79,99</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
-          corporis animi, veniam exercitationem voluptates commodi corrupti ab
-          ratione provident. Quaerat doloremque in incidunt unde, eaque
-          laboriosam. Tempore et at dolores!
-        </p>
+        <p>{product.description}</p>
 
         <button>Buy now</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: (price.unit_amount / 100).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }),
+      description: product.description,
+    },
+    revalidate: 60 * 60 * 1,
+  }
 }
